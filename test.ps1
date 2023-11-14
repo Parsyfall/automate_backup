e14f7673ca56d326b9f675c98d85ca4351a50e66
Tronciu Dumitru <72997097+Parsyfall@users.noreply.github.com>
2023-11-14 10:29:41 +0200

    Delete test.ps1

--- test.ps1
+++
@@ -1,6 +0,0 @@
-$time = Get-Date -UFormat %R -Minute 0 -Hour 0
-$action = New-ScheduledTaskAction -Execute python3 -Argument "C:\Users\Dumitru\Git\traineeland\assignment_2\script.py"
-$trigger = New-ScheduledTaskTrigger -Weekly -At $time
-Write-Output $trigger
-
-# Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Backup Script" -Description "Periodicaly back up files"
